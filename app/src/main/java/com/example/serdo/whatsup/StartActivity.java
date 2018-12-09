b2322732be33791665aa1806833f7c7b6bec6fce
serdo <serdoune.hamza@gmx.de>
2018-12-09 16:08:23 +0100

    Hallo

--- app/src/main/java/com/example/serdo/whatsup/StartActivity.java
+++ app/src/main/java/com/example/serdo/whatsup/StartActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class StartActivity extends AppCompatActivity {
@@ -15,12 +16,29 @@ public class StartActivity extends AppCompatActivity {
     FirebaseUser firebaseUser;
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //check if user is null
+        if (firebaseUser != null){
+            Intent intent = new Intent(StartActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
 
 
         login = findViewById(R.id.login);
